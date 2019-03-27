a8db68337be9f0edc2e5c5539b46ca097d5086ee
Simon Mika <simon@mika.se>
2019-03-27 13:25:04 +0100

    Fixed test.

--- source/DateTime.spec.ts
+++ source/DateTime.spec.ts
@@ -2,8 +2,8 @@ import * as model from "."
 
 describe("DateTime", () => {
 	it("create + is", () => {
-		const d = model.DateTime.create(new Date(2020, 11, 31, 23, 59, 59))
+		const d = model.DateTime.create(new Date(Date.UTC(2020, 11, 31, 23, 59, 59)))
 		expect(model.DateTime.is(d))
-		expect(d).toBe("2020-11-31T23:59:59")
+		expect(d).toBe("2020-12-31T23:59:59.000Z")
 	})
 })
