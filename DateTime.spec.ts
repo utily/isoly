88c1dee6bb3b84a0390695ec0dec6bac4be79eb4
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2020-03-12 11:50:46 +0100

    Fixed zero-padding of datetime

--- DateTime.spec.ts
+++ DateTime.spec.ts
@@ -5,10 +5,16 @@ describe("DateTime", () => {
 		["20 Jul 2019 10:30:40 GMT+2", "10:30:40"],
 		["21 Jul 2019 10:30:50 GMT", "12:30:50"],
 	]
+	const dataLocal = [
+		"4 Jul 2020 10:20:30 GMT", "2020-07-04 12:20:30 PM",
+	]
 	it("create + is", () => {
 		const d = model.DateTime.create(new Date(Date.UTC(2020, 11, 31, 23, 59, 59)))
 		expect(model.DateTime.is(d))
 		expect(d).toBe("2020-12-31T23:59:59.000Z")
+	})
+	it.skip("zero-pads localized", () => {
+		expect(model.DateTime.localize(new Date(dataLocal[0]), "sv-SE")).toEqual(dataLocal[1])
 	})
 	it.skip("localize with locale", () => {
 		for (const date of data) {
