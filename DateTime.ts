4c5d0d2c0eeef31f6cd2629a64d8581e9d76bcad
Simon Mika <simon@mika.se>
2020-06-25 17:21:02 +0200

    improved-datetime-2

--- DateTime.ts
+++ DateTime.ts
@@ -21,6 +21,6 @@ export namespace DateTime {
 			year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit",
 			timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
 		}
-		return (is(value) ? parse(value) : value).toLocaleDateString(localeString, localeOptions)
+		return (is(value) ? parse(value) : value).toLocaleString(localeString, localeOptions)
 	}
 }
