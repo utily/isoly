9a3c9a44e8bb1a3ce0ff5034ecf35f40eb2f1de8
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2020-09-28 13:59:39 +0200

    exporting calling code (#22)

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import * as CountryCode from "./CountryCode"
+import { CallingCode } from "./CallingCode"
 import { Currency } from "./Currency"
 import { CurrencyCode } from "./CurrencyCode"
 import { DateTime } from "./DateTime"
 import { Encoding } from "./Encoding"
 import { Language } from "./Language"
 
-export { CountryCode, Currency, CurrencyCode, DateTime, Encoding, Language }
+export { CallingCode, CountryCode, Currency, CurrencyCode, DateTime, Encoding, Language }
