79ea08820cc4073ee86a9dc246d0b44566101756
florianebmeier <70569666+florianebmeier@users.noreply.github.com>
2020-10-28 23:43:16 +0100

    Created Locale type (#25)
    * Created Locale type
    
    * eslintignore updated

--- index.ts
+++ index.ts
@@ -5,5 +5,6 @@ import { CurrencyCode } from "./CurrencyCode"
 import { DateTime } from "./DateTime"
 import { Encoding } from "./Encoding"
 import { Language } from "./Language"
+import { Locale } from "./Locale"
 
-export { CallingCode, CountryCode, Currency, CurrencyCode, DateTime, Encoding, Language }
+export { CallingCode, CountryCode, Currency, CurrencyCode, DateTime, Encoding, Language, Locale }
