77672a72c2308c695d1372c4c60ebac4b69819d2
qijieye <86956109+qijieye@users.noreply.github.com>
2021-09-02 23:54:02 +0200

    Added DateRange file and more functions in Date and unit tests. (#53)
    * Add DateRange and more functions in Date.
    
    * Added type DateSpan to function next and previous in Date. Improved the unit test of Date.
    
    * Added create function of DateRange and its corresponding unit test.
    
    * Try different test case.

---
+++ DateRange.spec.ts
@@ -0,0 +1,14 @@
+import * as model from "./DateRange"
+describe("DateRange", () => {
+	it("create", () => {
+		expect(model.DateRange.create("2021-01-01", "2020-01-01")).toEqual({ start: "2020-01-01", end: "2021-01-01" })
+		expect(model.DateRange.create("2001-01-01", { years: 1, months: 2, days: 3 })).toEqual({
+			start: "2001-01-01",
+			end: "2002-03-04",
+		})
+		expect(model.DateRange.create("2001-01-01", { years: -1, months: -1, days: -1 })).toEqual({
+			start: "1999-11-30",
+			end: "2001-01-01",
+		})
+	})
+})
