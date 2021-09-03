83ea4c69dcb24f02cfcbef5ba0b175e7a9fb9d01
Robin Nedergård <robin.nedergard@payfunc.com>
2021-09-03 11:23:39 +0200

    Parity for: Added DateRange file and more functions in Date and unit tests. (#53)
    * Add DateRange and more functions in Date.
    
    * Added type DateSpan to function next and previous in Date. Improved the unit test of Date.
    
    * Added create function of DateRange and its corresponding unit test.
    
    * Try different test case.

--- Date.spec.ts
+++ Date.spec.ts
@@ -6,6 +6,40 @@ describe("Date", () => {
 		expect(model.Date.is(d))
 		expect(d).toBe('2020-12-31')
 	})
+	it("next", () => {
+		expect(model.Date.next("2001-01-01")).toEqual("2001-01-02")
+		expect(model.Date.next("2001-01-01", 90)).toEqual("2001-03-31")
+		expect(model.Date.next("2001-01-01", { years: 1, months: 1, days: 1 })).toEqual("2002-02-02")
+	})
+	it("previous", () => {
+		expect(model.Date.previous("2001-01-01")).toEqual("2000-12-31")
+		expect(model.Date.previous("2001-01-01", 10)).toEqual("2000-12-22")
+		expect(model.Date.previous("2012-12-10", { years: 10, months: 12, days: 1 })).toEqual("2001-12-09")
+	})
+	it("nextMonth", () => {
+		expect(model.Date.nextMonth("2001-12-01")).toEqual("2002-01-01")
+		expect(model.Date.nextMonth("2021-03-31", 90)).toEqual("2028-10-01")
+	})
+	it("previousMonth", () => {
+		expect(model.Date.previousMonth("2001-01-01")).toEqual("2000-12-01")
+		expect(model.Date.previousMonth("2028-03-31")).toEqual("2028-03-02")
+	})
+	it("nextYear", () => {
+		expect(model.Date.nextYear("2001-12-01")).toEqual("2002-12-01")
+		expect(model.Date.nextYear("2002-02-01", 90)).toEqual("2092-02-01")
+	})
+	it("previousYear", () => {
+		expect(model.Date.previousYear("2001-01-01")).toEqual("2000-01-01")
+		expect(model.Date.previousYear("2001-03-01", 10)).toEqual("1991-03-01")
+	})
+	it("firstOfMonth", () => {
+		expect(model.Date.firstOfMonth("2002-12-21")).toEqual("2002-12-01")
+		expect(model.Date.firstOfMonth("2021-02-01")).toEqual("2021-02-01")
+	})
+	it("lastOfMonth", () => {
+		expect(model.Date.lastOfMonth("2001-01-01")).toEqual("2001-01-30")
+		expect(model.Date.lastOfMonth("2001-12-24")).toEqual("2001-12-30")
+	})
 	if (new Date(Date.UTC(2020, 11, 31, 23, 59, 59)).getTimezoneOffset() == -60) {
 		it("zero-pads localized", () => {
 			expect(model.Date.localize(new Date("4 Jul 2020 10:20:30 GMT"), "sv-SE")).toEqual("2020-07-04")
