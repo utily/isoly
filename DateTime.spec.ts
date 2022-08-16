479df60b8ddb2f484f029f8e70522dbfab8d1f6a
Amanda Hwatz Björkholm <amanda.hwatz@paxport.net>
2022-08-16 12:11:29 +0200

    Add formats to localize , add truncate, rework is

--- DateTime.spec.ts
+++ DateTime.spec.ts
@@ -3,8 +3,85 @@ import * as isoly from "./index"
 describe("DateTime", () => {
 	it("create + is", () => {
 		const d = isoly.DateTime.create(new Date(Date.UTC(2020, 11, 31, 23, 59, 59)))
-		expect(isoly.DateTime.is(d)).toEqual(true)
 		expect(d).toBe("2020-12-31T23:59:59.000Z")
+		expect(isoly.DateTime.is(d)).toEqual(true)
+	})
+	;(
+		[
+			["2019-04-01T01", new Date(2019, 3, 1, 1)],
+			["2019-04-01T01Z", new Date(Date.UTC(2019, 3, 1, 1))],
+			["2019-04-01T01+01:00", new Date(Date.UTC(2019, 3, 1, 0))],
+			["2019-04-01T01:11", new Date(2019, 3, 1, 1, 11)],
+			["2019-04-01T01:11Z", new Date(Date.UTC(2019, 3, 1, 1, 11))],
+			["2019-04-01T01:11+01:00", new Date(Date.UTC(2019, 3, 1, 0, 11))],
+			["2019-04-01T01:11:29", new Date(2019, 3, 1, 1, 11, 29)],
+			["2019-04-01T01:11:29Z", new Date(Date.UTC(2019, 3, 1, 1, 11, 29))],
+			["2019-04-01T01:11:29+01:00", new Date(Date.UTC(2019, 3, 1, 0, 11, 29))],
+			["2019-04-01T01:11:29.123", new Date(2019, 3, 1, 1, 11, 29, 123)],
+			["2019-04-01T01:11:29.123Z", new Date(Date.UTC(2019, 3, 1, 1, 11, 29, 123))],
+			["2020-12-31T23:59:59.999Z", new Date(Date.UTC(2020, 11, 31, 23, 59, 59, 999))],
+			["2019-04-01T01:11:29.123+01:00", new Date(Date.UTC(2019, 3, 1, 0, 11, 29, 123))],
+		] as [isoly.DateTime, Date][]
+	).forEach(([dateTime, date]) => {
+		it("parse " + dateTime, () => {
+			expect(isoly.DateTime.parse(dateTime)).toEqual(date)
+		})
+	})
+	;(
+		[
+			["milliseconds", "2019-04-01T01:11:29.123+01:00", "2019-04-01T01:11:29.123+01:00"],
+			["seconds", "2019-04-01T01:11:29+01:00", "2019-04-01T01:11:29.000+01:00"],
+			["minutes", "2019-04-01T01:11+01:00", "2019-04-01T01:11:00.000+01:00"],
+			["hours", "2019-04-01T01+01:00", "2019-04-01T01:00:00.000+01:00"],
+		] as const
+	).forEach(([precision, expected, padded]) => {
+		it("truncate " + precision, () => {
+			const dateTime = "2019-04-01T01:11:29.123+01:00"
+			const truncated = isoly.DateTime.truncate(dateTime, precision)
+			expect(truncated).toEqual(expected)
+			expect(isoly.DateTime.truncate(truncated, "milliseconds")).toEqual(padded)
+		})
+	})
+	;[
+		"2019-04-01T01",
+		"2019-04-01T01Z",
+		"2019-04-01T01+01:00",
+		"2019-04-01T01:11",
+		"2019-04-01T01:11Z",
+		"2019-04-01T01:11+01:00",
+		"2019-04-01T01:11:29",
+		"2019-04-01T01:11:29Z",
+		"2019-04-01T01:11:29+01:00",
+		"2019-04-01T01:11:29.000",
+		"2019-04-01T01:11:29.000Z",
+		"2020-12-31T23:59:59.999Z",
+		"2019-04-01T01:11:29.000+01:00",
+	].forEach(dateTime => {
+		it("is " + dateTime, () => expect(isoly.DateTime.is(dateTime)).toEqual(true))
+	})
+	;[
+		"2019-04-01T1",
+		"2019-04-01T01:Z",
+		"2019-04-01T01+25:00",
+		"2019-04-01T01:1",
+		"2019-04-01T01:1Z",
+		"2019-04-01T01:1+01:00",
+		"2019-04-01T01:11:29:",
+		"2019-04-01T01:11:29:Z",
+		"2019-04-01T01:11:2+01:00",
+		"2019-02-30T01:11:29.000",
+		"2019-04-01T01:11:29.00Z",
+		"2019-04-01T01:11:29.000+01:0",
+		"2019-04-01T24",
+		"2019-04-01T25Z",
+		"2019-04-01TA0+01:00",
+		"2019-04-01T01:60Z",
+		"2019-04-01T01:11:63",
+		"2019-04-01T01:99:29Z",
+		"2019-04-01T01:11:63+01:00",
+		"2019-04-01T01:11:63.000",
+	].forEach(dateTime => {
+		it("is not " + dateTime, () => expect(isoly.DateTime.is(dateTime)).toEqual(false))
 	})
 	it("epoch", () => {
 		expect(isoly.DateTime.epoch("2019-04-01T00:00:00.000Z")).toBe(1554076800)
@@ -21,15 +98,39 @@ describe("DateTime", () => {
 			it("localize with locale " + date[0], () =>
 				expect(isoly.DateTime.localize(new Date(date[0]), "sv-SE").split(" ")[1]).toEqual(date[1])
 			)
-
 		for (const date of data)
 			it("localize without locale " + date[0], () =>
 				expect(isoly.DateTime.localize(new Date(date[0])).split(" ")[1]).toEqual(date[1])
 			)
-
 		it("localize DateTime with locale", () => {
 			expect(isoly.DateTime.localize("2020-12-31T23:59:59.000Z", "en-US")).toEqual("01/01/2021, 12:59:59 AM")
 		})
+
+		it("localize 2020-12-31T23:59Z to en-US", () => {
+			expect(isoly.DateTime.localize("2020-12-31T23:59Z", "en-US")).toEqual("01/01/2021, 12:59 AM")
+		})
+		it("localize 2020-12-31T23Z to en-US", () => {
+			expect(isoly.DateTime.localize("2020-12-31T23Z", "en-US")).toEqual("01/01/2021, 12 AM")
+		})
+		it('localize 2020-12-31T23:12Z to en-US { month: "short", day: "numeric", hour: "numeric", minute: "numeric" }', () => {
+			expect(
+				isoly.DateTime.localize(
+					"2020-12-31T23:12Z",
+					{ month: "short", day: "numeric", hour: "numeric", minute: "numeric" },
+					"en-US"
+				)
+			).toEqual("Jan 1, 12:12 AM")
+		})
+		it('localize 2020-12-31T23:12Z to de-DE { month: "short", day: "numeric", hour: "numeric", minute: "numeric" }', () => {
+			expect(
+				isoly.DateTime.localize(
+					"2020-12-31T23:12Z",
+					{ month: "short", day: "numeric", hour: "numeric", minute: "numeric" },
+					"de-DE"
+				)
+			).toEqual("1. Jan., 00:12")
+		})
+
 		it("getDate", () => {
 			expect(isoly.DateTime.getDate("2020-12-31T23:59:59.000Z")).toEqual("2020-12-31")
 		})
@@ -58,6 +159,7 @@ describe("DateTime", () => {
 			const value = "2020-12-31T23:59:59.123Z"
 			const minutes = isoly.DateTime.truncate(value, "minutes")
 			expect(minutes).toEqual("2020-12-31T23:59Z")
+			// expect(isoly.DateTime.is(minutes)).toEqual(true)
 		})
 	}
 })
--- DateTime.ts
+++ DateTime.ts
@@ -6,16 +6,60 @@ import { TimeZone } from "./TimeZone"
 export type DateTime = string
 
 export namespace DateTime {
+	function isHours(v: string): boolean {
+		return (v[0] >= "0" && v[0] <= "1" && v[1] >= "0" && v[1] <= "9") || (v[0] == "2" && v[1] >= "0" && v[1] <= "3")
+	}
+	function isMinutes(v: string): boolean {
+		return v[0] >= "0" && v[0] <= "5" && v[1] >= "0" && v[1] <= "9"
+	}
+	function isSeconds(v: string): boolean {
+		return (v[0] >= "0" && v[0] <= "5" && v[1] >= "0" && v[1] <= "9") || v == "60" || v == "61"
+	}
 	export function is(value: any | DateTime): value is DateTime {
+		const length = value.length
+		// 2019-04-01T01
+		// 2019-04-01T01Z
+		// 2019-04-01T01+01:00
+		// 2019-04-01T01:11
+		// 2019-04-01T01:11Z
+		// 2019-04-01T01:11+01:00
+		// 2019-04-01T01:11:29
+		// 2019-04-01T01:11:29Z
+		// 2019-04-01T01:11:29+01:00
+		// 2019-04-01T01:11:29.000
+		// 2019-04-01T01:11:29.000Z
+		// 2019-04-01T01:11:29.000+01:00
+		// 01234567890123456789012345678
+		// 0         1         2
 		return (
 			typeof value == "string" &&
-			/^(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)$/.test(
-				value
-			)
+			length >= 13 &&
+			length <= 29 &&
+			Date.is(value.substring(0, 10)) &&
+			value[10] == "T" &&
+			isHours(value.substring(11, 13)) &&
+			(length == 13 ||
+				TimeZone.is(value.substring(13)) ||
+				(value[13] == ":" &&
+					length >= 16 &&
+					isMinutes(value.substring(14, 16)) &&
+					(length == 16 ||
+						TimeZone.is(value.substring(16)) ||
+						(value[16] == ":" &&
+							length >= 19 &&
+							isSeconds(value.substring(17, 19)) &&
+							(length == 19 ||
+								TimeZone.is(value.substring(19)) ||
+								(value[19] == "." &&
+									length >= 23 &&
+									[...value.substring(20, 23)].every(c => c >= "0" && c <= "9") &&
+									(length == 23 || TimeZone.is(value.substring(23)))))))))
+			// /^(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)$/.test(
+			// 	value
 		)
 	}
 	export function parse(value: DateTime): globalThis.Date {
-		return new globalThis.Date(value)
+		return new globalThis.Date(DateTime.truncate(value, "milliseconds"))
 	}
 	export function create(
 		value: number,
@@ -49,18 +93,52 @@ export namespace DateTime {
 	export function now(): DateTime {
 		return create(new globalThis.Date())
 	}
-	export function localize(value: DateTime | globalThis.Date, locale?: Locale, timeZone?: TimeZone): DateTime {
-		const localeString = locale ? locale : Intl.DateTimeFormat().resolvedOptions().locale
-		return (is(value) ? parse(value) : value).toLocaleString(localeString, {
-			year: "numeric",
-			month: "2-digit",
-			day: "2-digit",
-			hour: "2-digit",
-			minute: "2-digit",
-			second: "2-digit",
-			timeZone: timeZone ?? Intl.DateTimeFormat().resolvedOptions().timeZone,
-		})
+	export interface Format {
+		localeMatcher?: "best fit" | "lookup" | undefined
+		weekday?: "long" | "short" | "narrow" | undefined
+		era?: "long" | "short" | "narrow" | undefined
+		year?: "numeric" | "2-digit" | undefined
+		month?: "numeric" | "2-digit" | "long" | "short" | "narrow" | undefined
+		day?: "numeric" | "2-digit" | undefined
+		hour?: "numeric" | "2-digit" | undefined
+		minute?: "numeric" | "2-digit" | undefined
+		second?: "numeric" | "2-digit" | undefined
+		timeZoneName?: "short" | "long" | "shortOffset" | "longOffset" | "shortGeneric" | "longGeneric" | undefined
+		formatMatcher?: "best fit" | "basic" | undefined
+		hour12?: boolean | undefined
+		timeZone?: TimeZone | undefined
 	}
+	export function localize(value: DateTime | globalThis.Date, format: Format, locale?: Locale): string
+	export function localize(value: DateTime | globalThis.Date, locale?: Locale, timeZone?: TimeZone): string
+	export function localize(
+		value: DateTime | globalThis.Date,
+		locale?: Locale | Format,
+		timeZone?: TimeZone | Locale
+	): string {
+		let result: string
+		if (typeof locale == "object") {
+			const localeString = timeZone ? timeZone : Intl.DateTimeFormat().resolvedOptions().locale
+			result = (is(value) ? parse(value) : value).toLocaleString(localeString, locale as any)
+		} else {
+			const precision = is(value) ? DateTime.precision(value) : "milliseconds"
+			result = localize(
+				value,
+				{
+					year: "numeric",
+					month: "2-digit",
+					day: "2-digit",
+					hour: "2-digit",
+					minute:
+						precision == "minutes" || precision == "seconds" || precision == "milliseconds" ? "2-digit" : undefined,
+					second: precision == "seconds" || precision == "milliseconds" ? "2-digit" : undefined,
+					timeZone: timeZone as TimeZone,
+				},
+				locale
+			)
+		}
+		return result
+	}
+
 	export function timeZone(value: DateTime): TimeZone | "" {
 		const result = value[value.length - 1] == "Z" ? "Z" : value.substring(value.length - 6)
 		return TimeZone.is(result) ? result : ""
@@ -93,7 +171,7 @@ export namespace DateTime {
 		// eslint-disable-next-line prefer-const
 		let [date, time] = value.split("T", 2)
 		time = time.substring(0, time.length - zone.length)
-		switch (value.length) {
+		switch (time.length) {
 			case 2:
 				time += ":00:00.000"
 				break
