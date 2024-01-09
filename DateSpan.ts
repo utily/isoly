857dfa2d1a923e317610c7aa105225d5fcc5385f
Elias Eriksson <elias@issuefab.com>
2024-01-09 09:01:17 +0100

    Empty timespans (#136)
    * fix: time /date spans are allowed to be empty
    
    * tests
    
    * debugging
    
    * debugging
    
    * debugging
    
    * using node 20 in ci
    
    * removed debugging logs
    
    * brought back space

--- DateSpan.ts
+++ DateSpan.ts
@@ -8,10 +8,11 @@ export namespace DateSpan {
 	export function is(value: DateSpan | any): value is DateSpan {
 		return (
 			typeof value == "object" &&
+			!!value &&
+			!Array.isArray(value) &&
 			(typeof value.years == "number" || value.years == undefined) &&
 			(typeof value.months == "number" || value.months == undefined) &&
-			(typeof value.days == "number" || value.days == undefined) &&
-			(typeof value.years == "number" || typeof value.months == "number" || typeof value.days == "number")
+			(typeof value.days == "number" || value.days == undefined)
 		)
 	}
 }
--- TimeSpan.ts
+++ TimeSpan.ts
@@ -11,20 +11,15 @@ export namespace TimeSpan {
 	export function is(value: TimeSpan | any): value is TimeSpan {
 		return (
 			typeof value == "object" &&
+			!!value &&
+			!Array.isArray(value) &&
 			(typeof value.years == "number" || value.years == undefined) &&
 			(typeof value.months == "number" || value.months == undefined) &&
 			(typeof value.days == "number" || value.days == undefined) &&
 			(typeof value.hours == "number" || value.hours == undefined) &&
 			(typeof value.minutes == "number" || value.minutes == undefined) &&
 			(typeof value.seconds == "number" || value.seconds == undefined) &&
-			(typeof value.milliseconds == "number" || value.milliseconds == undefined) &&
-			(typeof value.years == "number" ||
-				typeof value.months == "number" ||
-				typeof value.days == "number" ||
-				typeof value.hours == "number" ||
-				typeof value.minutes == "number" ||
-				typeof value.seconds == "number" ||
-				typeof value.milliseconds == "number")
+			(typeof value.milliseconds == "number" || value.milliseconds == undefined)
 		)
 	}
 	export function toHours(value: TimeSpan, round?: Round): number {
