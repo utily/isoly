b9caf73dcf4182511de3e0f0f982a4483ce30e1d
Simon Mika <simonmika@users.noreply.github.com>
2025-01-10 13:25:56 +0100

    Fixed wrong imports. (#154)

--- Week/Parts.ts
+++ Week/Parts.ts
@@ -1,6 +1,6 @@
-import { DayOfWeek } from "DayOfWeek"
 import { isly } from "isly"
 import { Date } from "../Date"
+import { DayOfWeek } from "../DayOfWeek"
 import { Number } from "./Number"
 
 export interface Parts {
