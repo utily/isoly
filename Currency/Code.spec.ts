24204ae62db9586dba42b5f9565b5b2a1710524f
Simon Mika <simon@mika.se>
2025-01-10 21:12:27 +0100

    Improved coverage.

--- Currency/Code.spec.ts
+++ Currency/Code.spec.ts
@@ -2,9 +2,22 @@ import { isoly } from "../index"
 
 describe("Currency.Code", () => {
 	it("undefined", () => expect(isoly.Currency.Code.is(undefined)).toBeFalsy())
-	it("check types array", () => {
-		expect(isoly.Currency.Code.values.every(c => isoly.CurrencyCode.is(c))).toBeTruthy()
-		expect(isoly.Currency.Code.is("752")).toBeTruthy()
-		expect(isoly.Currency.Code.is("000")).toBeFalsy()
-	})
+	it.each([
+		["752", true],
+		["000", false],
+	] as const)("is(%s)", (code, expected) => expect(isoly.Currency.Code.is(code)).toBe(expected))
+	it.each([
+		["USD", "840"],
+		["EUR", "978"],
+		["JPY", "392"],
+		["XXX", "999"],
+		["INV" as any, undefined],
+	] as const)("from(%s) == %s", (currency, code) => expect(isoly.Currency.Code.from(currency)).toBe(code))
+	it.each([
+		["840", "USD"],
+		["978", "EUR"],
+		["392", "JPY"],
+		["999", "XXX"],
+		["000" as any, undefined],
+	] as const)("to(%s) == %s", (code, currency) => expect(isoly.Currency.Code.to(code)).toBe(currency))
 })
