831060a5ed3597abaa9e8d729b161feac7d5d62f
Simon Mika <simon@mika.se>
2025-01-12 17:39:08 +0100

    FIxed import issue.

--- TimeZone.ts
+++ TimeZone.ts
@@ -1,4 +1,4 @@
-import { TimeZoneOffset } from "TimeZoneOffset"
+import { TimeZoneOffset } from "./TimeZoneOffset"
 
 /** IANA format. Takes any time zone Stockholm, London and UTC is only examples. */
 export type TimeZone = "Europe/Stockholm" | "Europe/London" | "UTC" | (string & Record<never, never>) // The Record<never...> makes autocomplete work in your IDE.
