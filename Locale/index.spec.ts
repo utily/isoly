e93d1b74bed3981ce516c8a86f4f57bf09f56c7d
Simon Mika <simon@mika.se>
2025-01-12 16:12:50 +0100

    Test name cleanup.

--- Locale/index.spec.ts
+++ Locale/index.spec.ts
@@ -5,17 +5,17 @@ describe("Locale", () => {
 		["sv-SE", true],
 		["de-DE", true],
 		["de-SE", false],
-	] as const)("is %s == %s", (locale, expected) => expect(isoly.Locale.is(locale)).toEqual(expected))
+	] as const)("is(%s) == %s", (locale, expected) => expect(isoly.Locale.is(locale)).toEqual(expected))
 	it.each([
 		["sv-SE", "SE"],
 		["de-DE", "DE"],
 		["xx-XX" as any, undefined],
-	] as const)("toAlpha2 %s == %s", (locale, expected) => expect(isoly.Locale.toAlpha2(locale)).toEqual(expected))
+	] as const)("toAlpha2(%s) == %s", (locale, expected) => expect(isoly.Locale.toCountry(locale)).toEqual(expected))
 	it.each([
 		["sv-SE", "sv"],
 		["de-DE", "de"],
 		["xx-XX" as any, undefined],
-	] as const)("toLanguage %s == %s", (locale, expected) => expect(isoly.Locale.toLanguage(locale)).toEqual(expected))
+	] as const)("toLanguage(%s) == %s", (locale, expected) => expect(isoly.Locale.toLanguage(locale)).toEqual(expected))
 	it.each([
 		["sv", "SE", "sv-SE"],
 		["de", "DE", "de-DE"],
