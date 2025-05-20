42da796caf48f04a3d0d75daf893220ce931c208
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2025-05-20 14:50:38 +0200

    Fixed version error  by "downgrading" to major version 2. (#163)
    * Fixed version error in by "downgrading" to 2.
    
    * Fixed publish workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,20 @@
-/// <reference types="vitest/config" />
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
 	test: {
-		typecheck: {
-			tsconfig: "./tsconfig.json",
-		},
+		typecheck: { tsconfig: "./tsconfig.json" },
 		coverage: {
 			reporter: ["text", "json", "html"],
 			enabled: true,
 			all: true,
 			cleanOnRerun: true,
-			thresholds: {
-				statements: 99,
-				branches: 94,
-				functions: 95,
-				lines: 99,
-			},
+			thresholds: { statements: 99, branches: 94, functions: 95, lines: 99 },
 		},
 		globals: true,
 		include: ["**/*.spec.[tj]s"],
 		testTimeout: 20000,
 		isolate: false,
 		exclude: ["node_modules", "dist"],
-		server: {
-			deps: {
-				inline: ["isly"],
-			},
-		},
+		server: { deps: { inline: ["isly"] } },
 	},
 })
